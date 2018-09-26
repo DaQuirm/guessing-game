8e9dc3c5f1a185fd3e0f74f4011a06f7911dbd7e
DaQuirm <leo.daquirm@gmail.com>
2018-09-26 22:16:18 +0200

    Use an interpreted App type to separate IO

--- app/Main.hs
+++ app/Main.hs
@@ -1,20 +1,44 @@
 {-# LANGUAGE TypeApplications #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Main where
 
-import System.Random (randomRIO)
+import System.Random (RandomGen, randomR, randomRIO)
 
 import Lib
 
+data App
+  = PrintLine String App
+  | ReadLine (String -> App)
+  | Generate Int Int (Int -> App)
+  | End
+
+runApp :: App -> IO ()
+runApp End                        = pure ()
+runApp (PrintLine string next)    = do
+  putStrLn string
+  runApp next
+runApp (ReadLine handler)         = do
+  input <- getLine
+  runApp $ handler input
+runApp (Generate from to handler) = do
+  secret <- randomRIO (from, to) :: IO Int
+  runApp $ handler secret
+
+loop :: Int -> Int -> App
+loop secret 0        = PrintLine ("game over! it was " ++ show secret) End
+loop secret attempts =
+  PrintLine "Make a guess: " $
+    ReadLine $ \input ->
+      let guess = read @Int input
+       in case compare secret guess of
+            LT -> PrintLine "too big!" continue
+            GT -> PrintLine "too small!" continue
+            EQ -> PrintLine ("you won! it's indeed " ++ show secret ++ "!") End
+            where continue = loop secret $ attempts - 1
+
+app :: App
+app = Generate 1 10 $ \secret -> loop secret 3
+
 main :: IO ()
-main = do
-  secret <- randomRIO (1, 10) :: IO Int
-  logic secret 3
-    where logic secret 0        = putStrLn $ "game over! it was " ++ show secret
-          logic secret attempts = do
-            guess <- read @Int <$> getLine
-            case compare secret guess of
-              LT -> putStrLn "too big!"   >> continue
-              GT -> putStrLn "too small!" >> continue
-              EQ -> putStrLn $ "you won! it's indeed " ++ show secret ++ "!"
-              where continue = logic secret $ attempts - 1
+main = runApp app
