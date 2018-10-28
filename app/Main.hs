4d5540d77dee3af6a02f4b980265e59af677b74f
DaQuirm <leo.daquirm@gmail.com>
2018-10-28 21:06:41 +0100

    𐅖 Use mtl-style

--- app/Main.hs
+++ app/Main.hs
@@ -1,58 +1,49 @@
-{-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE NamedFieldPuns #-}
 {-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleContexts #-}
 
 module Main where
 
 import System.Random (RandomGen, randomR, randomRIO)
+import Control.Monad.State.Lazy (MonadState(get, put), evalStateT, modify)
+import Control.Monad.IO.Class (MonadIO, liftIO)
 
-import Free(Free(..), liftF)
+type AppState = Int -- number of attempts
 
-data AppF a
-  = PrintLine String a
-  | ReadLine (String -> a)
-  | Generate Int Int (Int -> a)
-  deriving Functor
+printLine :: (MonadIO m) => String -> m ()
+printLine = liftIO . putStrLn
 
-type App = Free AppF
+readLine :: (MonadIO m) => m String
+readLine = liftIO getLine
 
-runApp :: App a -> IO ()
-runApp (Pure a)    = pure ()
-runApp (Free (PrintLine string next)) = do
-  putStrLn string
-  runApp next
-runApp (Free (ReadLine handler)) = do
-  input <- getLine
-  runApp $ handler input
-runApp (Free (Generate from to handler)) = do
-  secret <- randomRIO (from, to) :: IO Int
-  runApp $ handler secret
+generate :: (MonadIO m) => Int -> Int -> m Int
+generate from to = liftIO $ randomRIO (from, to)
 
-printLine :: String -> App ()
-printLine string = liftF $ PrintLine string ()
+loop :: (MonadIO m, MonadState AppState m) => Int -> m ()
+loop secret = do
+  attempts <- get
+  if attempts == 0 then
+    printLine $ "game over! it was " ++ show secret
+  else do
+    printLine "Make a guess: "
+    input <- readLine
+    let guess = read input
+     in case compare secret guess of
+          LT -> printLine "too big!"   >> continue
+          GT -> printLine "too small!" >> continue
+          EQ -> printLine $ "you won! it's indeed " ++ show secret ++ "!"
+          where
+            continue = do
+              modify $ subtract 1
+              loop secret
 
-readLine :: App String
-readLine = liftF $ ReadLine id
-
-generate :: Int -> Int -> App Int
-generate from to = liftF $ Generate from to id
-
-loop :: Int -> Int -> App ()
-loop secret 0        = printLine $ "game over! it was " ++ show secret
-loop secret attempts = do
-  printLine "Make a guess: "
-  input <- readLine
-  let guess = read @Int input
-   in case compare secret guess of
-        LT -> printLine "too big!"   >> continue
-        GT -> printLine "too small!" >> continue
-        EQ -> printLine $ "you won! it's indeed " ++ show secret ++ "!"
-        where continue = loop secret $ attempts - 1
-
-app :: App ()
+app :: (MonadIO m, MonadState AppState m) => m ()
 app = do
   secret <- generate 1 10
-  loop secret 3
+  loop secret
+
+attempts_per_game :: Int
+attempts_per_game = 3
 
 main :: IO ()
-main = runApp app
+main = evalStateT app attempts_per_game
